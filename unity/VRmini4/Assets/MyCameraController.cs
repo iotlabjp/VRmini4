d6168aeee5362092482f8241331e31034813ea24
Yohei Sawayama <yohei.sawayama@gmail.com>
2016-08-06 01:51:41 +0900

    add unity

---
+++ unity/VRmini4/Assets/MyCameraController.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+
+public class MyCameraController : MonoBehaviour {
+
+	void Start (){
+		Input.gyro.enabled = true;
+	}
+
+    // Update is called once per frame
+    void Update () {
+        Quaternion gyro = Input.gyro.attitude;
+        this.transform.localRotation = Quaternion.Euler(90, 0, 0) * (new Quaternion(-gyro.x, -gyro.y, gyro.z, gyro.w));
+    }
+}
